67150ba4804edf4687429c2f8938332a3f02690c
Purav Kapoor <73545057+Alpha-Chax@users.noreply.github.com>
2022-10-30 22:59:46 +0530

    Add files via upload
    Backend

---
+++ index.js
@@ -0,0 +1,30 @@
+const connectToMongo = require('./db');
+const express = require('express')
+var cors = require('cors')
+const path = require('path')
+
+connectToMongo();
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+//Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
+//--------------DEVELOPMENT----------------------------
+__dirname = path.resolve();
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname,'/Client/build')));
+  app.get("*", (req,res) =>{
+    res.sendFile(path.resolve(__dirname, "Client", "build", "index.html"))
+  })
+}
+
+//--------------DEVELOPMENT-----------------------------
+
+app.listen(port, () => {
+  console.log(`iNotebook backend listening on PORT ${port}`)
+})
